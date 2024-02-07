36e7db16f287299a73a1768d76943df3cc21dee9
Chamakasadupa <150304496+Chamakasadupa@users.noreply.github.com>
2024-02-07 21:57:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['94742678276', '94764829228'] 
+global.prems = ['94742678276', '94764829228']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '꧁ ꜱʟ | 𝐂 ｚａｄｕｐａ ☛☚ ꧂3 𝛁'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'ᄃΉΛMΛKΛ ZΛDЦPΛ' 
+global.author = '@ᄃんﾑﾶﾑズﾑ 乙ﾑりひｱﾑ' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
